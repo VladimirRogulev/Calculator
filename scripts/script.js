75df4d89e80537c6f0cf17a3cd70fa8f997c5877
VladimirRogulev <vladimir.rogulev@gmail.com>
2020-02-09 15:27:14 +0300

    rework

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,46 @@
-function insert(item) {
-    document.form.textview.value =  document.form.textview.value + item;
+let result = 0; 
+let act = '';
+let sign = false;
+
+function action(action) {
+	act = action;
+	sign = false;
+	result = parseInt(window.innerHTML);
 }
 
-function equal () {
-    let exp =  document.form.textview.value;
-    if (exp) {
-        document.form.textview.value = eval(exp);
-    }
+function number(num) {
+
+    const window = document.getElementById("window");
+
+    if(window.innerHTML.length < 12){
+
+        if(window.innerHTML != 0 && sign == true ){
+            window.innerHTML += num;
+        } else	{
+            window.innerHTML = num;
+            sign = true; 
+        }
+    }   
+}
+
+function equal() {
+    
+    if(act != ''){		
+        switch(act){
+            case '/': 
+                result = result / parseInt(window.innerHTML);
+                break;
+            case '*': 
+                result = result * parseInt(window.innerHTML);
+                break;
+            case '-': 
+                result = result - parseInt(window.innerHTML);
+                break;
+            case '+': 
+                result = result + parseInt(window.innerHTML);
+                break;
+        }
+        window.innerHTML = result;
+        act = ''; 
+    } 
 }
