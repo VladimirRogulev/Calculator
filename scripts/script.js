4b3b4b5aabc0e077e411a835f0e795615474dce8
VladimirRogulev <vladimir.rogulev@gmail.com>
2020-02-26 20:51:13 +0300

    bug fix

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,30 @@ const screen = document.getElementById("window");
 let result;
 let act;
 let sign = false;
+let point = false;
 
 const action = (action) => {
 	act = action;
-	sign = false;
-	result = parseInt(screen.innerHTML);
+    sign = false;
+    point = false;
+	result = Number(screen.innerHTML);
+};
+
+const dot = (dot) => {
+    if(screen.innerHTML.length < 12){
+        if(!point){
+            screen.innerHTML += dot;
+            point = true;
+            sign = true;
+        }
+    }
 };
 
 const number = (num) => {
     if(screen.innerHTML.length < 12){
-        if(screen.innerHTML !== '0' && sign === true ){
+        if(screen.innerHTML !== '0' && sign){
             screen.innerHTML += num;
-        } else	{
+        } else {
             screen.innerHTML = num;
             sign = true;
         }
@@ -24,20 +36,37 @@ const equal = () => {
     if(act !== undefined){
         switch(act){
             case '/':
-                result /= parseInt(screen.innerHTML);
+                result /= Number(screen.innerHTML);
                 break;
             case '*':
-                result *= parseInt(screen.innerHTML);
+                result *= Number(screen.innerHTML);
                 break;
             case '-':
-                result -= parseInt(screen.innerHTML);
+                result -= Number(screen.innerHTML);
                 break;
             case '+':
-                result += parseInt(screen.innerHTML);
+                result += Number(screen.innerHTML);
                 break;
         }
 
-        screen.innerHTML = result;
-        act = undefined;
+        if (String(+result.toFixed(10)).length < 13){
+            screen.innerHTML = +result.toFixed(10);
+            act = undefined;
+        } else {
+            screen.innerHTML = "0";
+            alert("Число не может поменстится на экран!"); 
+        }
     }
 };
+
+const clean = () => {
+    screen.innerHTML = '0';
+    result = 0;
+    act = '';
+    sign = false;
+    point = false;
+}
+
+const back = () => {
+    screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length - 1);
+}
